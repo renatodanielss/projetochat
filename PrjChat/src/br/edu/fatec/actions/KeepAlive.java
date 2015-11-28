697263856f08f7962a391b13d640e463b59f01f2
may-silva-nascimento <may.silva.nascimento@gmail.com>
2015-11-28 21:44:10 -0200

    Teste

---
+++ PrjChat/src/br/edu/fatec/actions/KeepAlive.java
@@ -0,0 +1,38 @@
+package br.edu.fatec.actions;
+
+import org.json.JSONArray;
+
+public class KeepAlive {
+	private String action;
+	private String nickname;
+	private JSONArray users;
+	
+	public String getAction() {
+		return action;
+	}
+	
+	public void setAction(String action) {
+		this.action = action;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public JSONArray getUsers() {
+		return users;
+	}
+
+	public void setUsers(JSONArray users) {
+		this.users = users;
+	}
+
+	@Override
+	public String toString() {
+		return "[action=" + this.action + ", nickname=" + this.nickname + ", users=" + this.users + "]";
+    }
+}
